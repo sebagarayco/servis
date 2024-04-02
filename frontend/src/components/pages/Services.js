56994157f1c45009cfa696b1a20a49090a3dbf7f
sebagarayco <sebagarayco@gmail.com>
2024-04-02 18:30:07 -0300

    Services Page

--- frontend/src/components/pages/Services.js
+++ frontend/src/components/pages/Services.js
@@ -1,22 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Nav from '../layout/Nav';
 import ServisSpinner from '../utils/ServisSpinner';
-import { Row, Col, Container } from 'react-bootstrap';
-
+import { Row, Col, Container, Card, Button } from 'react-bootstrap';
+// Actions
+import { getServices } from '../../redux/actions/services';
+// Redux
+import { connect } from 'react-redux';
 
 export class Services extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: true,
+		};
+	}
+
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({ loading: false });
+		}, 1000);
+
+		this.props.getServices();
+	}
 	render() {
 		return (
 			<div>
 				<Nav />
-				<Row>
-					<Col>
-
-					</Col>
-				</Row>
+				<Container>
+					<Row xs={1} md={2} lg={3} className="g-4">
+						{this.state.loading ? (
+							<ServisSpinner />
+						) : (
+							this.props.services.map(service => (
+								<Col key={service.id}>
+									<Card>
+										<Card.Img variant="top" src={service.user.image} />
+										<Card.Body>
+											<Card.Title>{service.description}</Card.Title>
+											<Card.Text>{service.subcategory.name}</Card.Text>
+											<Card.Text>
+												<strong>Precio por hora:</strong> ${service.hourly_price}
+											</Card.Text>
+											<Card.Text>
+												<strong>Precio por día completo:</strong> ${service.full_day_price}
+											</Card.Text>
+											<Button variant="primary">Contratar</Button>
+										</Card.Body>
+									</Card>
+								</Col>
+							))
+						)}
+					</Row>
+				</Container>
 			</div>
-		)
+		);
 	}
 }
 
-export default Services
+const mapStateToProps = state => ({
+	services: state.services.services,
+});
+
+export default connect(mapStateToProps, { getServices })(Services);
